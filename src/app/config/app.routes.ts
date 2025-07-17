f7433a8026a28a3a583879a52f74b293d3ad8603
Lucas Cruz <lucas.araujo@lgepartner.com>
2025-07-17 11:18:47 -0400

    feat: profile

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from '../user/login/login';
 import { Register } from '../user/register/register';
 import { LadingPage } from '../pages/lading-page/lading-page';
+import { Profile } from '../user/profile/profile';
 
 export const routes: Routes = [
     {path:'', component: LadingPage},
     {path:'login', component: Login},
-    {path:'register', component: Register}
+    {path:'register', component: Register},
+    {path:'profile', component: Profile}
 ];
