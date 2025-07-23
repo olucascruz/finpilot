fb20d80ac5fefb7db6870c05d23af819a2ad3de4
Lucas Cruz <olucascruz.developer@gmail.com>
2025-07-23 17:36:05 -0400

    create: admin

---
+++ src/app/admin/admin/admin.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-admin',
+  imports: [MatButtonModule],
+  templateUrl: './admin.html',
+  styleUrl: './admin.scss'
+})
+export class Admin {
+  
+}
--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -3,10 +3,13 @@ import { Login } from '../user/login/login';
 import { Register } from '../user/register/register';
 import { LadingPage } from '../pages/lading-page/lading-page';
 import { Profile } from '../user/profile/profile';
+import { Admin } from '../admin/admin/admin';
 
 export const routes: Routes = [
     {path:'', component: LadingPage},
     {path:'login', component: Login},
     {path:'register', component: Register},
-    {path:'profile', component: Profile}
+    {path:'profile', component: Profile},
+    {path:'admin', component: Admin}
+
 ];
